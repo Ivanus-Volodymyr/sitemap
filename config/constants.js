00df5a74734224133fe380bc67339bd977e22895
Ivanus Volodymyr <91600635+Ivanus-Volodymyr@users.noreply.github.com>
2024-12-31 10:07:48 +0200

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 1340,
-    API: process.env.API || 'https://test-api.skilliant.net',
+    API: process.env.API || 'https://test-api.skilliant.net ',
 };
